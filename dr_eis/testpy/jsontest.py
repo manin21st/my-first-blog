be8acb961240cafe860c89bd3c32dcde99f1665c
bhsong <bhsong@stlsoft.co.kr>
2019-01-21 23:47:47 +0900

    jsontest commit

---
+++ dr_eis/testpy/jsontest.py
@@ -0,0 +1,30 @@
+import json
+
+# 테스트용 Python Dictionary
+customer = {
+    'id': 152352,
+    'name': '강진수',
+    'history': [
+        {'date': '2015-03-11', 'item': 'iPhone'},
+        {'date': '2016-02-23', 'item': 'Monitor'},
+    ]
+}
+
+# JSON 인코딩(파이썬 오브젝트를 텍스트 타입으로 변환)
+jsonString = json.dumps(customer)
+
+# 문자열 출력
+print(jsonString)
+print(type(jsonString))  # class str
+
+# 테스트용 JSON 문자열
+jsonString = '{"name": "강진수", "id": 152352, "history": [{"date": "2015-03-11", "item": "iPhone"}, {"date": "2016-02-23", "item": "Monitor"}]}'
+
+# JSON 디코딩 (텍스트를 파이썬 오브젝트 타입으로 변환)
+dict = json.loads(jsonString)
+
+# Dictionary 데이타 체크
+print(dict['name'])
+
+for h in dict['history']:
+    print(h['date'], h['item'])
